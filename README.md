997a5b0c4bc4cc0fe94ecd985d370a42ba3febe9
JoyDas1902 <ijoydas1902@gmail.com>
2024-01-20 23:42:41 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+## Mern App
+
+First, install the dependencies:
+
+```bash
+npm install
+# or
+yarn install
+# or
+pnpm install
+# or
+bun install
+```
+
+### create a .env file with your own MONGO, AUTH_SECRET, AUTH_URL, GITHUB_ID, GITHUB_SECRET
+
+run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import { auth } from "@/lib/auth";
+import Link from "next/link";
+import Links from "./links/Links";
+import styles from "./navbar.module.css";
+
+const Navbar = async () => {
+  const session = await auth();
+
+  return (
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>
+        Logo
+      </Link>
+      <div>
+        <Links session={session} />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
